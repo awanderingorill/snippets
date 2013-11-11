f39fa2ba1d770510e87d6339403db716b1e4cece
Ana <agiraldow@gmail.com>
2013-11-10 19:24:30 -0500

    initialized snippets app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnippetsApp::Application.config.secret_token = 'c4fd27f0b0aaf5e73e681f802333473d4a25417747dffde7c2e2f0e32dc5ef4068cae9b8ed2e69cd3b3667605ca4faaea03ba170f1b118c0294a1263e14f869a'
