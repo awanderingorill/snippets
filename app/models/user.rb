b63f4ef420b5e97ab9d8085e5ac6fc57a091ce15
Ana <agiraldow@gmail.com>
2013-11-15 09:35:20 -0500

    stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@
 class User < ActiveRecord::Base
   #validation
   before_save do
-    self.name = self.name.downcase
+    self.email = self.email.downcase
   end
   #validation
   attr_accessible :name, :email, :password, :password_confirmation, :country
